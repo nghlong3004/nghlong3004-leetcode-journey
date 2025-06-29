74dcaa98c76f09fc009160fc94f709a636fe7eda
nghlong3004 <nghlong3004@gmail.com>
2025-06-30 04:49:14 +0700

    perf: optimize N-Queens solution for performance

--- src/main/java/org/nghlong3004/backtracking/NQueens.java
+++ src/main/java/org/nghlong3004/backtracking/NQueens.java
@@ -1,87 +1,81 @@
 package org.nghlong3004.backtracking;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 class Solution {
 
-	private boolean[][] matrix = new boolean[16][16];
+	private boolean[] isMainDiagonal;
+	private boolean[] isAntiDiagonal;
+	private boolean[] isColumn;
+	private List<List<String>> result;
 
-	private boolean isDiagonal(int i, int j, int n) {
-		for (int a = i + 1, b = j + 1; a < n && b < n; ++a, ++b) {
-			if (matrix[a][b]) {
-				return true;
-			}
-		}
-		for (int a = i - 1, b = j - 1; a >= 0 && b >= 0; --a, --b) {
-			if (matrix[a][b]) {
-				return true;
-			}
-		}
-		for (int a = i + 1, b = j - 1; a < n && b >= 0; ++a, --b) {
-			if (matrix[a][b]) {
-				return true;
-			}
-		}
-		for (int a = i - 1, b = j + 1; a >= 0 && b < n; --a, ++b) {
-			if (matrix[a][b]) {
-				return true;
-			}
-		}
-		return false;
+	public Solution() {
+		result = new ArrayList<List<String>>();
+		isMainDiagonal = new boolean[24];
+		isAntiDiagonal = new boolean[24];
+		isColumn = new boolean[12];
 	}
 
-	private boolean isColumn(int j, int n) {
-		for (int i = 0; i < n; ++i) {
-			if (matrix[i][j]) {
-				return true;
-			}
-		}
-		return false;
+	private boolean isDiagonal(int i, int j) {
+		return isMainDiagonal[i - j + 12] || isAntiDiagonal[i + j];
 	}
 
 	private boolean isChoose(int i, int j, int n) {
-		if (isColumn(j, n) || isDiagonal(i, j, n)) {
+		if (isColumn[j] || isDiagonal(i, j)) {
 			return false;
 		}
 		return true;
 	}
 
-	private List<Character> initQueen(int n) {
-		List<Character> queen = new ArrayList<Character>(Collections.nCopies(n, '.'));
-		return queen;
+	private List<String> convertDataToInformation(List<Integer> data) {
+
+		List<String> infomation = new ArrayList<String>();
+		for (int i = 0; i < data.size(); ++i) {
+			StringBuilder builder = new StringBuilder();
+			for (int j = 0; j < data.size(); ++j) {
+				if (j == data.get(i)) {
+					builder.append('Q');
+				} else {
+					builder.append('.');
+				}
+			}
+			infomation.add(builder.toString());
+		}
+		return infomation;
 	}
 
-	private List<List<String>> backtracking(int i, int n, List<Character> queen, List<String> queens,
-			List<List<String>> nQueens) {
-		if (i == n) {
-			nQueens.add(new ArrayList<String>(queens));
-			return nQueens;
+	private void backtracking(int row, int n, List<Integer> queens) {
+		if (row == n) {
+			result.add(convertDataToInformation(queens));
 		}
-		for (int j = 0; j < n; ++j) {
-			if (isChoose(i, j, n)) {
-				queen.set(j, 'Q');
-				queens.add(queen.stream().map(String::valueOf).collect(Collectors.joining()));
-				matrix[i][j] = true;
-				backtracking(i + 1, n, initQueen(n), queens, nQueens);
-				queen.set(j, '.');
+		for (int column = 0; column < n; ++column) {
+			if (isChoose(row, column, n)) {
+				queens.add(column);
+				isMainDiagonal[row - column + 12] = true;
+				isAntiDiagonal[row + column] = true;
+				isColumn[column] = true;
+				backtracking(row + 1, n, queens);
 				queens.remove(queens.size() - 1);
-				matrix[i][j] = false;
+				isMainDiagonal[row - column + 12] = false;
+				isAntiDiagonal[row + column] = false;
+				isColumn[column] = false;
 			}
 		}
-		return nQueens;
 	}
 
 	public List<List<String>> solveNQueens(int n) {
-		return backtracking(0, n, initQueen(n), new ArrayList<String>(), new ArrayList<List<String>>());
+		backtracking(0, n, new ArrayList<Integer>());
+		return result;
 	}
 }
 
 public class NQueens {
 	public static void main(String[] args) {
-		Solution solution = new Solution();
-		System.out.println(solution.solveNQueens(4));
+		try (Scanner scanner = new Scanner(System.in)) {
+			Solution solution = new Solution();
+			System.out.println(solution.solveNQueens(scanner.nextInt()));
+		}
 	}
 }
